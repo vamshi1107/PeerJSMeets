5292dc92d94fc344cc3e2b6b2a9492cbc977a17d
Karthik Reddy Anumula <anumulakarthikreddy12345@gmail.com>
2023-11-09 01:26:59 +0530

    2 nd commit

--- src/PeerVideo.js
+++ src/PeerVideo.js
@@ -10,39 +10,64 @@ export default function Peervideo() {
     const incommingVideo = useRef(null)
     const outgoingVideo = useRef(null)
     const [peerId,setPeerId] = useState(null)
+    const currentPeer = useRef(null)
+    const [remotePeerIdValue, setRemotePeerIdValue] = useState('');
 
 
     useEffect(() => {
+        var peer = new Peer();
+        
+        peer.on('open', (id)=>{
+            console.log(id)
+            setPeerId(id)
+        })
         
 
+        peer.on('call',(call)=>{
+
+            var getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
+
+            setinitialView('startCall')
+            getUserMedia({ video: true, audio: true }, (mediaStream) => {
+                outgoingVideo.current.srcObject = mediaStream;
+                outgoingVideo.current.play();
+                call.answer(mediaStream)
+                console.log("call meta data",call.metadata," /n ",call)
+                setRemotePeerIdValue(call.peer)
+                call.on('stream',(remoteStream)=>{
+                    incommingVideo.current.srcObject = remoteStream
+                    incommingVideo.current.play();
+                })
+            });
+
+        })
+
+        currentPeer.current = peer;
+        
 
     }, [])
 
     const onInputSubmitOrButtonClick = () => {
-        console.log(meetId?.current?.value, "  jjjs")
+        console.log(remotePeerIdValue, "  jjjs")
         setinitialView('startCall')
-        var getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
-        var peer = new Peer();
-        
-        peer.on('open', (peerid)=>{
-            setPeerId(peerId)
-        })
 
-        
-        getUserMedia({ video: true, audio: true }, (mediaStream) => {
+        var getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
+        console.log("hello")
+        getUserMedia({ video: true, audio: true},(mediaStream)=>{
             outgoingVideo.current.srcObject = mediaStream;
             outgoingVideo.current.play();
-            peer.on('call',(call)=>{
-                call.answer(mediaStream)
-                call.on('stream', function(remoteStream) {
-                incommingVideo.current.srcObject = remoteStream
-                incommingVideo.current.play();
-                });
-    
-    
-            })
             
-        });
+            const call = currentPeer.current.call(remotePeerIdValue,mediaStream)
+
+            call.on('stream',(remoteStream)=>{
+                incommingVideo.current.srcObject = remoteStream;
+                incommingVideo.current.play();
+            })
+
+        })
+        
+        
+
     }
 
 
@@ -57,11 +82,11 @@ export default function Peervideo() {
                 <div className='contianerMain'><p>Have a Meeting ID Use it to enter a meet</p>
                     <form onSubmit={() => onInputSubmitOrButtonClick()}>
                         <label style={{ marginRight: '8px' }}>Please Enter the meeting id</label>
-                        <input ref={meetId} style={{ marginRight: '8px' }}></input>
+                        <input onChange={e => setRemotePeerIdValue(e.target.value)} style={{ marginRight: '8px' }}></input>
                         <button onClick={() => { onInputSubmitOrButtonClick() }}>Submit</button>  </form></div>
             </div> :
                 initalView == 'startCall' ? <div >
-                    <div style={{ display: "flex", justifyContent: "center", marginTop: "80px", marginBottom: "24px" }}> Welcome to Call Meet Id<b>{meetId?.current?.value}</b></div>
+                    <div style={{ display: "flex", justifyContent: "center", marginTop: "80px", marginBottom: "24px" }}> Welcome to Call Meet Id<b style={{marginLeft: "4px"}}>{remotePeerIdValue}</b></div>
                     <div style={{ display: "flex", alignItems: "center", flexDirection: "column" }}>
                         <div className='incomingVideo'>
                             <video className='videoContainer' ref={incommingVideo}></video>

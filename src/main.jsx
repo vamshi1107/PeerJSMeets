51b0aa6d34f1446f5c1d75eec1dc290c04e8775a
Anumula Karthik Reddy <54942989+190030089@users.noreply.github.com>
2023-11-10 14:35:19 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
-import PeerVideo from "./PeerVideo.jsx";
+import PeerVideo from "./PeerVideo.js";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
-import { PeerProvider } from "./Context.jsx";
-import Navbar from "./Navbar.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
